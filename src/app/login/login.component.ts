7e86f8e4c0ae78560addbb7b5816b0f6c9a4d187
handrawanw <handrawanw@gmail.com>
2021-08-23 21:44:19 +0700

    update

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import {HttpClient} from "@angular/common/http";
+import { baseUrl } from '../store';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -7,10 +9,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  formsLogin:any={
+    email:"",
+    password:""
+  };
 
-  ngOnInit(): void {
+  responseLogin:any={
+    error:0,
+    message:""
+  };
 
+  constructor(private http:HttpClient) { }
+
+  ngOnInit(): void {}
+
+  LoginRequest():void {
+    this.http.post(`${baseUrl}/users/login`,this.formsLogin).toPromise().then((data:any)=>{
+      this.responseLogin={
+        error:1,
+        message:data.message
+      };
+    }).catch((err:any)=>{
+      console.log(err)
+      if(err.error&&Array.isArray(err.error.message)){
+        this.responseLogin={
+          error:0,
+          message:"Error : "+err.statusText
+        };
+      }else{
+        this.responseLogin={
+          error:0,
+          message:"Error : "+err.error.message
+        };
+      }
+    });
   }
 
 }
--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
+import {HttpClient} from "@angular/common/http";
+import { baseUrl } from '../store';
+
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
@@ -7,10 +10,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() {}
+  formsRegister={
+    username:"",
+    email:"",
+    password:"",
+    no_hp:""
+  };
+
+  responseRegister={
+    error:0,
+    message:""
+  };
+
+  constructor(private http:HttpClient) {}
 
   ngOnInit(): void {
     
+  }
+
+  registerRequest():void {
+    this.http.post(`${baseUrl}/users/register`,this.formsRegister).toPromise().then((data:any)=>{
+      console.log(data)
+      this.responseRegister={
+        error:1,
+        message:data.message
+      };
+    }).catch((err:any)=>{
+      console.log(err);
+      if(err.error&&Array.isArray(err.error.message)){
+        this.responseRegister={
+          error:0,
+          message:"Error : "+err.statusText
+        };
+      }else{
+        this.responseRegister={
+          error:0,
+          message:"Error : "+err.error.message
+        };
+      }
+    });
   }
 
 }
